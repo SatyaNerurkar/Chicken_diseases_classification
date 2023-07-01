37822ee5b704cc560cafc7187f5794497372d0c7
SatyaNerurkar <satyanerurkar@gmail.com>
2023-07-02 01:04:20 +0530

    data ingestion completed

--- src/CNNClassifier/utils/common.py
+++ src/CNNClassifier/utils/common.py
@@ -3,9 +3,11 @@ from box.exceptions import BoxValueError
 import yaml
 from CNNClassifier import logger
 import json
+import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
-from pathlib import Pathfrom typing import Any
+from pathlib import Path
+from typing import Any
 import base64
 
 
